fdf8739d4bae1b3d2e0063e6b3fcf8ff4d1581a9
subodhac21 <subodhacharya21@gmail.com>
2023-03-13 20:59:50 +0545

    folder added

---
+++ item_page/app.js
@@ -0,0 +1,23 @@
+'use strict';
+const contact = document.querySelector('.contact');
+const cat = document.querySelector('.categories');
+const searchBar = document.querySelector(".search-bar");
+const login = document.querySelector('.login');
+const cart = document.querySelector('.cart');
+const search = document.querySelector(".search");
+const searchResult = document.querySelector(".search-result");
+const faTimes = document.querySelector(".fa-times");
+
+
+search.addEventListener("click",()=>{
+    searchBar.classList.toggle("show-searchbar");
+    login.classList.toggle("show-login");
+    cart.classList.toggle("show-cart");
+    search.classList.toggle("show-search");
+})
+faTimes.addEventListener("click",()=>{
+    searchBar.classList.toggle("show-searchbar");
+    login.classList.toggle("show-login");
+    cart.classList.toggle("show-cart");
+    search.classList.toggle("show-search");
+})

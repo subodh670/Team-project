1d8d06b54d3aac04c0214b51fc2a9fb7ead3f75b
subodhac21 <subodhacharya21@gmail.com>
2023-04-14 20:20:08 +0545

    two pages updated extra

---
+++ sign_up_page/script.js
@@ -0,0 +1,25 @@
+'use strict';
+const bars = document.querySelector(".bars");
+const contact = document.querySelector('.contact');
+const cat = document.querySelector('.categories');
+const searchBar = document.querySelector(".search-bar");
+const login = document.querySelector('.login');
+const cart = document.querySelector('.cart');
+const search = document.querySelector(".search");
+const searchResult = document.querySelector(".search-result");
+const faTimes = document.querySelector(".fa-times");
+
+
+search.addEventListener("click",()=>{
+    searchBar.classList.toggle("show-searchbar");
+    login.classList.toggle("show-login");
+    cart.classList.toggle("show-cart");
+    search.classList.toggle("show-search");
+})
+faTimes.addEventListener("click",()=>{
+    searchBar.classList.toggle("show-searchbar");
+    login.classList.toggle("show-login");
+    cart.classList.toggle("show-cart");
+    search.classList.toggle("show-search");
+})
+

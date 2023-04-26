f9992111c5fa149053035f6244cd1ee504894f6d
Subodh <subodhacharya21@gmail.com>
2023-04-26 14:07:27 +0545

    ajax updated

--- landing_page/product.php
+++ landing_page/product.php
@@ -1,11 +1,14 @@
 <?php
 
-include("../connectionPHP/connect.php");
-$sql = "SELECT * FROM PRODUCT";
-$array = oci_parse($conn, $sql);
-oci_execute($array);
-$rows = oci_fetch_all($array, $a);
+
 // echo $rows;
-echo json_encode($a);
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    include("../connectionPHP/connect.php");
+    $sql = "SELECT * FROM PRODUCT";
+    $array = oci_parse($conn, $sql);
+    oci_execute($array);
+    $rows = oci_fetch_all($array, $a);
+    echo json_encode($a);
+}
 
 ?>
--- otp_page/otp.php
+++ otp_page/otp.php
@@ -1,14 +1,16 @@
 <?php
-session_start();
-$email = $_GET['email'];
-// $email = 'subodhacharya21@gmail.com';
-include("../connectionPHP/connect.php");
-$sql = "SELECT C_OTP FROM CUSTOMER WHERE C_EMAILADDRESS = '$email'";
-$array = oci_parse($conn, $sql);
-oci_execute($array);
-$row = oci_fetch_array($array);
-$arr = $row[0];
-echo json_encode($arr);
 
+if($_SERVER['REQUEST_METHOD']=="POST"){
+    session_start();
+    $email = $_GET['email'];
+    // $email = 'subodhacharya21@gmail.com';
+    include("../connectionPHP/connect.php");
+    $sql = "SELECT C_OTP FROM CUSTOMER WHERE C_EMAILADDRESS = '$email'";
+    $array = oci_parse($conn, $sql);
+    oci_execute($array);
+    $row = oci_fetch_array($array);
+    $arr = $row[0];
+    echo json_encode($arr);
+}
 
 ?>
